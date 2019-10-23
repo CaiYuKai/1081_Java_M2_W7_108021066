341cbe7b64d70da7f285e95db271e77035bb7bc6
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-10-23 14:58:41 +0800

    2 commit

---
+++ src/Main02.java
@@ -0,0 +1,11 @@
+public class Main02{
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int sum=0;
+        for (int i=3;i<=n;i+=3){
+            sum=sum+i;
+        }
+        System.out.println(sum);
+    }
+}
